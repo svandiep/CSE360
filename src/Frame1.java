6ebc24e22f6eab899b4af824027296c03e5bf165
DESKTOP-5T9MU5L\Scott <svandiep@asu.edu>
2018-09-23 19:28:41 -0700

    GUI initial

---
+++ src/Frame1.java
@@ -0,0 +1,129 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.TextArea;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Frame1 {
+
+	private JFrame frmNetworkPathAnalyzer;
+	private JTable table_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame1 window = new Frame1();
+					window.frmNetworkPathAnalyzer.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Frame1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmNetworkPathAnalyzer = new JFrame();
+		frmNetworkPathAnalyzer.setTitle("Network Path Analyzer");
+		frmNetworkPathAnalyzer.setBounds(100, 100, 587, 459);
+		frmNetworkPathAnalyzer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmNetworkPathAnalyzer.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Calculate");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Cycle detected!");
+			}
+		});
+		btnNewButton.setBounds(449, 329, 97, 25);
+		frmNetworkPathAnalyzer.getContentPane().add(btnNewButton);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.setBounds(340, 329, 97, 25);
+		frmNetworkPathAnalyzer.getContentPane().add(btnClear);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(37, 60, 509, 240);
+		frmNetworkPathAnalyzer.getContentPane().add(scrollPane);
+		
+		table_1 = new JTable();
+		scrollPane.setViewportView(table_1);
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[]  {
+				"Activity", "Duration", "Dependencies"
+			}
+		));
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmNetworkPathAnalyzer.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNewDiagram = new JMenuItem("New Diagram");
+		mnFile.add(mntmNewDiagram);
+		
+		JMenuItem mntmCalculate = new JMenuItem("Calculate");
+		mnFile.add(mntmCalculate);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0); 
+			}
+		});
+		mnFile.add(mntmExit);
+		
+		JMenu mnAbout = new JMenu("About");
+		menuBar.add(mnAbout);
+		
+		JMenuItem mntmVersion = new JMenuItem("Version");
+		mnAbout.add(mntmVersion);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmUserGuide = new JMenuItem("User Guide");
+		mnHelp.add(mntmUserGuide);
+	}
+}
---
+++ src/Paths.java
@@ -0,0 +1,84 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+public class Paths extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JScrollPane scrollPane;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Paths frame = new Paths();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Paths() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Close");
+		btnNewButton.setBounds(323, 215, 97, 25);
+		contentPane.add(btnNewButton);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(23, 13, 386, 189);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"Path", "Total Duration"
+			}
+		));
+	}
+}

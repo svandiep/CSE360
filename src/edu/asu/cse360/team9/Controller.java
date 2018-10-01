85f848c67294e13a034570ec6b3da8ba32ae5f3a
Brandon Dressman <bdressma@asu.edu>
2018-09-30 18:59:42 -0700

    Modified proof of concept code to include the Controller. This is more realistic to final product.

--- src/edu/asu/cse360/team9/Controller.java
+++ src/edu/asu/cse360/team9/Controller.java
@@ -57,7 +57,29 @@ public class Controller {
 	
 	public ArrayList<Result> getResults()
 	{
+		ArrayList<Result> output = new ArrayList<Result>();
+		Result r1 = new Result();
+		r1.addPathNode("A", 2);
+		r1.addPathNode("B", 4);
+		r1.addPathNode("C", 5);
+		r1.addPathNode("E", 3);
+		output.add(r1);
+		
+		Result r2 = new Result();
+		r2.addPathNode("A", 2);
+		r2.addPathNode("F", 5);
+		r2.addPathNode("D", 6);
+		r2.addPathNode("E", 3);
+		output.add(r2);
+		
+		Result r3 = new Result();
+		r3.addPathNode("A", 2);
+		r3.addPathNode("B", 4);
+		r3.addPathNode("D", 6);
+		r3.addPathNode("E", 3);
+		output.add(r3);
+		
 		// Called by Paths JFrame on spawn to get the data for population of JTable.
-		return null;
+		return output;
 	}
 }

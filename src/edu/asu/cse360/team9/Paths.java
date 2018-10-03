ed7a8cb945ee80caaca5192c9795215e35675988
Brandon Dressman <bdressma@asu.edu>
2018-10-03 04:17:34 -0700

    Updated Paths to have a title, and load the Result collection. Currently displaying our dummy data, but should be wired up for when actual results are available.

--- src/edu/asu/cse360/team9/Paths.java
+++ src/edu/asu/cse360/team9/Paths.java
@@ -58,7 +58,7 @@ public class Paths extends JFrame {
 		// Keep reference to controller so the results can be polled later.
 		this.controller = controller;
 		
-		
+		this.setTitle("Path Analysis");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 600, 335);
 		contentPane = new JPanel();
@@ -81,22 +81,14 @@ public class Paths extends JFrame {
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		model = new DefaultTableModel(
-				new Object[][] {
-				{"", ""}
-			},
-			new String[] {
-				"Path", "Duration"
-			}
-		);
+		
 		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"", ""},
-			},
+			null,				// Do not want any rows initially for this table.
 			new String[] {
 				"Path", "Duration"
 			}
 		) {
+			private static final long serialVersionUID = 1L;
 			boolean[] columnEditables = new boolean[] {
 				false, false
 			};
@@ -104,6 +96,8 @@ public class Paths extends JFrame {
 				return columnEditables[column];
 			}
 		});
+		model = (DefaultTableModel) table.getModel();
+		
 		table.getColumnModel().getColumn(0).setPreferredWidth(200);
 		table.getColumnModel().getColumn(1).setPreferredWidth(62);
 		table.getColumnModel().getColumn(1).setMinWidth(10);
@@ -120,6 +114,11 @@ public class Paths extends JFrame {
 			});
 		
 		// We will use the populated data to fill the table
+		// Currently using dummy data for showing example.
 		ArrayList<Result> results = controller.getResults();
+		for(Result r : results)
+		{
+			model.addRow(new Object[] { r.getPath(), r.getDuration() } );
+		}
 	}
 }

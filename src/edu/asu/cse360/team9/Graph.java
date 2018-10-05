a1a4c3ed24c2ad5e1b32ea3ea680726db46019e6
Brandon Dressman <bdressma@asu.edu>
2018-10-05 01:25:06 -0700

    Tidy up Graph with comments, and removal of comments. No significant logic change.

--- src/edu/asu/cse360/team9/Graph.java
+++ src/edu/asu/cse360/team9/Graph.java
@@ -2,18 +2,30 @@ package edu.asu.cse360.team9;
 import java.util.*;
 import java.util.Map.Entry;
 
-/*
- * This class is the graph representation
- * 
+/**
+ * Provides representation of a directed graph as an adjacency list.
  * 
  */
 public class Graph {
 
+	// Maintained list of existing edges to prevent duplication.
 	private ArrayList<String> edges;
+	
+	// Adjacency list maps the activity name to the linked list.
 	private HashMap<String, LinkedList<Edge>> adjList;
+	
+	// Multiple start nodes require an internal start node that all provided
+	// start nodes will depend on. This is stripped out of any final results
+	// further up the call stack.
 	public static final String START_ACTIVITY = "__START__";
+	
+	// The stop activity has nothing depending on it, and there can only be one.
 	private String stopActivity = "";
 	
+	/**
+	 * Container for source and destination nodes to produce a directed edge.
+	 * 
+	 */
 	private class Edge
 	{
 		String source;
@@ -38,6 +50,15 @@ public class Graph {
 		edges = new ArrayList<String>();
 	}
 	
+	/**
+	 * Add a new edge to the correct index of the adjacency list
+	 * and add those previously unseen activities as keys.
+	 * 
+	 * @param source Where this edge starts
+	 * @param destination Where this edge goes
+	 * 
+	 * @throws IllegalArgumentException When the edge exists
+	 */
 	public void addEdge(String source, String destination)
 	{	
 		if(edges.contains(source + "-" + destination))
@@ -59,25 +80,14 @@ public class Graph {
 		edges.add(source + "-" + destination);
 	}
 	
-	// DEBUGGING FUNCTION - TO BE REMOVED
-	public void display()
-	{
-		 for(Entry<String, LinkedList<Edge>> m: adjList.entrySet()){
-		        System.out.print(m.getKey()+"-->");
-		        
-		        String spacer = "";
-		        for(Edge e : m.getValue())
-		        {
-		        	System.out.print(spacer + e);
-		        	spacer = " --> ";
-		        }
-		        System.out.println();
-		 }
-		 
-		 //System.out.println("Start: " + startActivity);
-		 System.out.println("Stop:  " + stopActivity);
-	}
-	
+	/**
+	 * Identify all paths between start and final nodes and their total
+	 * duration as a Result, which is collected into a List, and returned.
+	 * 
+	 * Utilizes depth-first search algorithm to perform calculation.
+	 * 
+	 * @param durations Map<String, Integer> where key is activity.
+	 */
 	public ArrayList<Result> getPaths(HashMap<String, Integer> durations)
 	{
 		ArrayList<Result> results = new ArrayList<>();
@@ -87,13 +97,13 @@ public class Graph {
 			visited.put(s, false);
 		}
 		ArrayList<String> path = new ArrayList<>();
-		//path.add(startActivity);
 		path.add(START_ACTIVITY);
 		
 		List<List<String>> paths = new ArrayList<List<String>>();
-		//DFS(paths, startActivity, visited, path);
 		DFS(paths, START_ACTIVITY, visited, path);
-				
+		
+		// Allows for test code to not need to provide durations when that feature
+		// is not under test.
 		if(durations != null)
 		{
 			for(List<String> list : paths)
@@ -106,12 +116,31 @@ public class Graph {
 				results.add(r);
 			}
 		}
+		// Sorts list in descending order by duration.
 		Collections.sort(results);
-		
-		// NOTE: To pass the tests, this must return them in descending order by duration.
 		return results;
 	}
 	
+	/**
+	 * Recursive implementation of depth-first search algorithm to identify all paths between
+	 * start and final nodes.
+	 * 
+	 * Algorithm:
+	 * Set current node to visited
+	 * If current node is the stop activity, then path has been found, so add to output
+	 * For all edges in this node's adjacency list:
+	 * 		If current edge's destination node is not visited, add it to path
+	 * 		recur on destination
+	 * 		Remove current edge's destination node from path.
+	 * Set current node to not visited
+	 * 
+	 * @param output The final result
+	 * @param current The current activity
+	 * @param visited Map<String, Boolean> where key is activity and boolean is if it has been visited.
+	 * @param path The current path in question.
+	 * 
+	 * @throws IllegalStateException when a visited node is reached, which is a cycle.
+	 */
 	private void DFS(List<List<String>> output, String current, Map<String, Boolean> visited, List<String> path) {
 		
 		visited.put(current, true);
@@ -137,16 +166,14 @@ public class Graph {
 		
 		visited.put(current, false);
 	}
-	
-	/*
-	public void setStartActivity(String startActivity)
-	{
-		if(this.startActivity.isEmpty())
-			this.startActivity = startActivity;
-		else
-			throw new IllegalArgumentException("Start activity was previously configured");
-	}
-	*/
+
+	/**
+	 * Allows calling code to set the stop activity only if it has not been set before.
+	 * 
+	 * @param stopActivity What the stop activity should be.
+	 * 
+	 * @throws IllegalArgumentException When the stop activity has previously been set.
+	 */
 	public void setStopActivity(String stopActivity)
 	{
 		if(this.stopActivity.isEmpty())

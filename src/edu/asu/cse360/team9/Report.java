fe339138992af439cf93e3e466bf89c84dd79fc1
DESKTOP-5T9MU5L\Scott <svandiep@asu.edu>
2018-10-28 20:11:19 -0700

    Added Jfile chooser button

--- src/edu/asu/cse360/team9/Report.java
+++ src/edu/asu/cse360/team9/Report.java
@@ -7,11 +7,21 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Dialog.ModalityType;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Report extends JDialog {
 
@@ -22,7 +32,33 @@ public class Report extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	private JTextField textField;
 	private JTextField textField_1;
+	public JButton btnNewButton;
 	
+	protected void saveToFile() {
+	    JFileChooser fileChooser = new JFileChooser();
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+	    fileChooser.setFileFilter(filter);
+	    int retval = fileChooser.showSaveDialog(btnNewButton);
+	    if (retval == JFileChooser.APPROVE_OPTION) {
+	      File file = fileChooser.getSelectedFile();
+	      if (file == null) {
+	        return;
+	      }
+	      if (!file.getName().toLowerCase().endsWith(".txt")) {
+	        file = new File(file.getParentFile(), file.getName() + ".txt");
+	      }
+	      try {
+	    	  textField.write(new OutputStreamWriter(new FileOutputStream(file),
+	            "utf-8"));
+	        Desktop.getDesktop().open(file);
+	      } catch (Exception e) {
+	        e.printStackTrace();
+	      }
+	    }
+	  }
+	/**
+	 *  Close the Dialog 
+	 */
 	public void CloseFrame(){
 	    super.dispose();
 	}
@@ -41,22 +77,31 @@ public class Report extends JDialog {
 		contentPanel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Report Name:");
-		lblNewLabel.setBounds(44, 38, 91, 16);
+		lblNewLabel.setBounds(33, 38, 91, 16);
 		contentPanel.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(146, 35, 192, 22);
+		textField.setBounds(131, 35, 192, 22);
 		contentPanel.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Save To:");
-		lblNewLabel_1.setBounds(44, 112, 56, 16);
+		lblNewLabel_1.setBounds(33, 112, 56, 16);
 		contentPanel.add(lblNewLabel_1);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(146, 109, 192, 22);
+		textField_1.setBounds(131, 109, 192, 22);
 		contentPanel.add(textField_1);
 		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Browse...");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				saveToFile();
+			}
+		});
+		btnNewButton.setBounds(335, 108, 97, 24);
+		contentPanel.add(btnNewButton);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
